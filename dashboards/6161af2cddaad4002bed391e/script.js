54710c5006dbdf96f20d8d5dd9d96ce8327070c0
Sisense POC <admin%40sisensepoc.com>
2022-12-15 13:17:09 +0000

    Initial commit

---
+++ dashboards/6161af2cddaad4002bed391e/script.js
@@ -0,0 +1,8 @@
+dashboard.on('widgetready',function(d) {
+   //Card view
+   //$('.dashboard-layout').css('background-color', '#F0F0F0');
+   //$('.dashboard-layout-cell-horizontal-divider').remove();
+   //$('.dashboard-layout-subcell-host').css('padding', '5');
+   //$('.dashboard-layout').css('padding-right', '10px');
+   //$('.dashboard-layout').css('padding-left', '10px');
+});
